65c7ddf49954c952d83da5fb1f0b185d7349b296
Lu Liu <nudtlliu@gmail.com>
2017-08-25 23:43:04 +0200

    remove the unused instructions control

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-if (!L.mapbox) throw new Error('include mapbox.js before air.js');
+if (!L.mapbox) throw new Error("include mapbox.js before air.js");
 
-L.air = require('./src/directions');
-L.air.format = require('./src/format');
-L.air.layer = require('./src/layer');
-L.air.inputControl = require('./src/input_control');
-L.air.errorsControl = require('./src/errors_control');
-L.air.routesControl = require('./src/routes_control');
-L.air.instructionsControl = require('./src/instructions_control');
-L.air.tracksControl = require('./src/tracks_control.js');
+L.air = require("./src/directions");
+L.air.format = require("./src/format");
+L.air.layer = require("./src/layer");
+L.air.inputControl = require("./src/input_control");
+L.air.errorsControl = require("./src/errors_control");
+L.air.routesControl = require("./src/routes_control");
+// L.air.instructionsControl = require('./src/instructions_control');
+L.air.tracksControl = require("./src/tracks_control.js");
